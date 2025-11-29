3540a4b317f07e7222187522e11ac33ebb2f18bf
Joshua Gilstrap <josh.gilstrap@icloud.com>
2025-11-29 14:58:44 -0500

    initiated xcode project

---
+++ WetBulb/WetBulb/WetBulbApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetBulbApp.swift
+//  WetBulb
+//
+//  Created by Joshua Gilstrap on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct WetBulbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
